32c2bdad878e565729bd199a86d45174132cf33a
Priyavrat Misra <PriyavratMisra123@gmail.com>
2021-04-14 13:59:48 +0530

    refactor code
    add code to modify networks' last layer

---
+++ networks.py
@@ -0,0 +1,34 @@
+import torch
+import torchvision
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def get_vgg16(pretrained=False, out_features=None):
+    model = torchvision.models.vgg16(pretrained=pretrained)
+    if out_features is not None:
+        model.classifier = torch.nn.Sequential(
+            *list(model.classifier.children())[:-1],
+            torch.nn.Linear(in_features=4096, out_features=out_features)
+        )
+
+    return model.to(device)
+
+
+def get_resnet18(pretrained=False, out_features=None):
+    model = torchvision.models.resnet18(pretrained=pretrained)
+    if out_features is not None:
+        model.fc = torch.nn.Linear(in_features=512, out_features=out_features)
+
+    return model.to(device)
+
+
+def get_densenet121(pretrained=False, out_features=None):
+    model = torchvision.models.densenet121(pretrained=pretrained)
+    if out_features is not None:
+        model.classifier = torch.nn.Linear(
+            in_features=1024, out_features=out_features
+        )
+
+    return model.to(device)
---
+++ utils.py
@@ -0,0 +1,96 @@
+import torch
+import numpy as np
+from tqdm import tqdm
+import torchvision.transforms as transforms
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+dirs = {
+    'train': 'data/COVID-19_Radiography_Dataset/train',
+    'val': 'data/COVID-19_Radiography_Dataset/val',
+    'test': 'data/COVID-19_Radiography_Dataset/test'
+}
+
+transform = {
+    'train': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+}
+
+
+def get_num_correct(preds, labels):
+    return preds.argmax(dim=1).eq(labels).sum().item()
+
+
+def fit(epochs, model, criterion, optimizer, train_dl, valid_dl):
+    valid_loss_min = np.Inf
+    len_train = 21005
+    len_valid = 80
+
+    for epoch in range(epochs):
+        train_loss, train_correct = 0, 0
+        train_loop = tqdm(train_dl)
+
+        model.train()
+        for batch in train_loop:
+            images, labels = batch[0].to(device), batch[1].to(device)
+            preds = model(images)
+            loss = criterion(preds, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * labels.size(0)
+            train_correct += get_num_correct(preds, labels)
+
+            train_loop.set_description(f'Epoch [{epoch+1:2d}/{epochs}]')
+            train_loop.set_postfix(
+                loss=loss.item(), acc=train_correct/len_train
+            )
+
+        model.eval()
+        with torch.no_grad():
+            valid_loss = 0
+            for batch in valid_dl:
+                images, labels = batch[0].to(device), batch[1].to(device)
+                preds = model(images)
+                loss = criterion(preds, labels)
+                valid_loss += loss.item() * labels.size(0)
+
+            train_loss = train_loss/len_train
+            valid_loss = valid_loss/len_valid
+            train_loop.write(f'\n\t\tAvg train loss: {train_loss:.6f}', end='')
+            train_loop.write(f'\tAvg valid loss: {valid_loss:.6f}')
+
+            # save model if validation loss has decreased
+            if valid_loss <= valid_loss_min:
+                train_loop.write('\t\tvalid_loss decreased', end=' ')
+                train_loop.write(f'({valid_loss_min:.6f} -> {valid_loss:.6f})')
+                train_loop.write('\t\tsaving model...')
+                torch.save(
+                    model.state_dict(),
+                    f'models/lr3e-5_{type(model).__name__.lower()}_{device}.pt'
+                )
+                valid_loss_min = valid_loss

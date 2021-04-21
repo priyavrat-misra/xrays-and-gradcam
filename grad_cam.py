219acadad6ae5f5ec392a49d65307fd4fd7b2c13
Priyavrat Misra <PriyavratMisra123@gmail.com>
2021-04-21 20:17:35 +0530

    add code for grad-cam

---
+++ grad_cam.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+
+class GradCAM:
+    def __init__(self, model, target_layer):
+        self.model = model.eval()
+        self.featuremaps = []
+        self.gradients = []
+
+        target_layer.register_forward_hook(self.save_featuremaps)
+        target_layer.register_full_backward_hook(self.save_gradients)
+
+    def save_featuremaps(self, module, input, output):
+        self.featuremaps.append(output)
+
+    def save_gradients(self, module, grad_input, grad_output):
+        self.gradients.append(grad_output[0])
+
+    def get_cam_weights(self, grads):
+        return np.mean(grads, axis=(1, 2))
+
+    def __call__(self, image, target=None):
+        preds = self.model(image)
+        self.model.zero_grad()
+
+        if target is None:
+            target = preds.argmax(dim=1).item()
+
+        preds[:, target].backward()
+
+        featuremaps = self.featuremaps[-1].cpu().data.numpy()[0, :]
+        gradients = self.gradients[-1].cpu().data.numpy()[0, :]
+
+        weights = self.get_cam_weights(gradients)
+        cam = np.zeros(featuremaps.shape[1:], dtype=np.float32)
+
+        for i, w in enumerate(weights):
+            cam += w * featuremaps[i]
+
+        cam = np.maximum(cam, 0)
+        cam = cv2.resize(cam, image.shape[-2:][::-1])
+        cam = cam - np.min(cam)
+        cam = cam / np.max(cam)
+        return cam
